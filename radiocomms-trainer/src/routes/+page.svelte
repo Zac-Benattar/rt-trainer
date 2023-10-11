dc2adbf6e237befcae25c6367f91d07927fbe809
Zac <zbenattar@gmail.com>
2023-10-11 10:47:03 +0100

    Added basic page links

--- radiocomms-trainer/src/routes/+page.svelte
+++ radiocomms-trainer/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let exampleScenarioId = 1;
+</script>
+
+<h1>Welcome to RT Trainer</h1>
+
+<p>RT Trainer is a web application that allows you to practice your radio communication skills.</p>
+<h2><a href={`/scenario/${exampleScenarioId}`}>Example Scenario</a></h2>
+<a href="/about">About</a>
+
--- radiocomms-trainer/src/routes/about/+page.svelte
+++ radiocomms-trainer/src/routes/about/+page.svelte
@@ -1,1 +1,2 @@
 <h1>About me</h1>
+<a href="/">Home</a>
