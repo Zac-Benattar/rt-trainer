5e3220fbb6383bbdf6a4153110cbf40c00a4e458
Zac <zbenattar@gmail.com>
2023-10-12 19:12:09 +0100

    Added logic to add parameters to generation url

--- radiocomms-trainer/src/routes/scenario/[scenarioId]/+page.svelte
+++ radiocomms-trainer/src/routes/scenario/[scenarioId]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { page } from '$app/stores'
     const scenarioId = $page.params.scenarioId
+    const unexpectedEvents = $page.url.searchParams.get('unexpectedEvents')
 </script>
 
 <h1>Scenario {scenarioId}</h1>
