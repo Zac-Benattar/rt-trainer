a1d63bd9d728bb8d2addccf349b38e68fa80c502
Zac <zbenattar@gmail.com>
2023-10-12 16:22:48 +0100

    Improved navbar element

---
+++ radiocomms-trainer/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<nav>
+  <ul class="navbar">
+    <li class="navbar">
+      <a class="navbar" href="/">Home</a>
+    </li>
+    <li class="navbar">
+      <a class="navbar" href="/profile">Profile</a>
+    </li>
+  </ul>
+</nav>
+
+<slot />
