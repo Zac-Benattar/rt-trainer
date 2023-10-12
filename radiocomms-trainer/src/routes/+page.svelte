170b259f239d08068a55e1b2cafa870d0d82e0b1
Zac <zbenattar@gmail.com>
2023-10-12 10:03:53 +0100

    Updated scenario generate button

--- radiocomms-trainer/src/routes/+page.svelte
+++ radiocomms-trainer/src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
-    export let exampleScenarioId = 1;
+    import { goto } from '$app/navigation';
+    export let exampleScenarioId = Math.floor(Math.random() * 1000000);
+
+    const handleClick = () => {
+        goto('/scenario/' + exampleScenarioId);
+    }
 </script>
 
 <h1>Welcome to RT Trainer</h1>
 
 <p>RT Trainer is a web application that allows you to practice your radio communication skills.</p>
-<h2><a href={`/scenario/${exampleScenarioId}`}>Example Scenario</a></h2>
+<button on:click={handleClick}>Generate Scenario </button>
 <a href="/about">About</a>
 
