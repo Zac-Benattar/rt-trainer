e152ea7fb37b2e00e063b11977a96dd24142d0b6
Zac <zbenattar@gmail.com>
2023-10-16 14:23:15 +0100

    Moved further sections to simulator component

--- my-skeleton-app/src/routes/scenario/[scenarioId]/+page.svelte
+++ my-skeleton-app/src/routes/scenario/[scenarioId]/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import Simulator from './Radio.svelte';
 	import { page } from '$app/stores';
-	import { clipboard } from '@skeletonlabs/skeleton';
-	import { SlideToggle } from '@skeletonlabs/skeleton';
 	var scenarioId = $page.params.scenarioId;
 
 	// check whether unexpected events are enabled
@@ -11,39 +9,11 @@
 	if (unexpectedEventsString != null) {
 		unexpectedEvents = unexpectedEventsString === 'True';
 	}
-
-	// generate the link to the scenario
-	var scenarioLink = 'www.rt-trainer.com/scenario/' + scenarioId;
-	if (unexpectedEvents) {
-		scenarioLink += '?unexpectedEvents=' + unexpectedEvents;
-	}
 </script>
 
 <div>
 	<div>
-		<h1>Scenario {scenarioId}</h1>
-	</div>
-	<div>
-		<p>
-			Unexpected events <SlideToggle
-				id="enable-random-events"
-				name="slider-small"
-				checked={unexpectedEvents}
-				active="bg-primary-500"
-				size="sm"
-				on:click={() => (unexpectedEvents = !unexpectedEvents)}
-			/>
-		</p>
-	</div>
-	<div>
-		<!-- Source -->
-		<div data-clipboard="scenarioLinkElement">{scenarioLink}</div>
-
-		<!-- Trigger -->
-		<button use:clipboard={{ element: 'scenarioLinkElement' }}>Copy</button>
-	</div>
-	<div>
 		<!-- No seed value works for some reason -->
-		<Simulator seed={scenarioId}/>
+		<Simulator seed={scenarioId} unexpectedEvents={unexpectedEvents}/>
 	</div>
 </div>
--- my-skeleton-app/src/routes/scenario/[scenarioId]/Simulator.svelte
+++ my-skeleton-app/src/routes/scenario/[scenarioId]/Simulator.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
 	import Radio from './Radio.svelte';
 	import Transponder from './Transponder.svelte';
+    import { clipboard } from '@skeletonlabs/skeleton';
+	import { SlideToggle } from '@skeletonlabs/skeleton';
+	export let unexpectedEvents: boolean = false;
     export let seed = "0";
+
+    // generate the link to the scenario
+	var scenarioLink = 'www.rt-trainer.com/scenario/' + seed;
+	if (unexpectedEvents) {
+		scenarioLink += '?unexpectedEvents=' + unexpectedEvents;
+	}
 </script>
 
 <div>
 	<div>
 		<h1>Simulator</h1>
+	</div>
+	<div>
+		<p>
+			Unexpected events <SlideToggle
+				id="enable-random-events"
+				name="slider-small"
+				checked={unexpectedEvents}
+				active="bg-primary-500"
+				size="sm"
+				on:click={() => (unexpectedEvents = !unexpectedEvents)}
+			/>
+		</p>
+	</div>
+	<div>
+		<!-- Source -->
+		<div data-clipboard="scenarioLinkElement">{scenarioLink}</div>
+
+		<!-- Trigger -->
+		<button use:clipboard={{ element: 'scenarioLinkElement' }}>Copy</button>
 	</div>
 	<div class="radio-transponder-container">
 		<slot {Radio} {Transponder}/>
