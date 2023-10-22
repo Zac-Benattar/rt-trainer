3822037b86e67e774884322662b422e78a244da0
Zac <zbenattar@gmail.com>
2023-10-22 16:50:06 +0100

    Started on radio display
    Radio display layout now correct
    Beginning to implement frequency selection

--- rt-trainer-app/src/lib/Simulator/RadioDisplay.svelte
+++ rt-trainer-app/src/lib/Simulator/RadioDisplay.svelte
@@ -4,9 +4,9 @@
 	export let DisplayOn: boolean = false;
 	export let mode: string = 'COM';
 	export let DigitSelected: number = 0;
-    export let radioSelectedFrequency: number = 1000;
-	export let radioAlternateFrequency: number = 2000;
-	export let radioTertiaryFrequency: number = 3000;
+	export let radioSelectedFrequency: number = 126.410;
+	export let radioAlternateFrequency: number = 123.170;
+	export let radioTertiaryFrequency: number = 179.200;
 
 	let mounted: boolean = false;
 	let digitArr = [1, 0, 0, 0];
@@ -15,6 +15,16 @@
 	$: {
 		if (!DisplayOn) {
 			mode = 'COM';
+		}
+	}
+	$: {
+		let newDigitArr = `${radioSelectedFrequency}`.split('').map(Number);
+		for (let i = 0; i < 4; i++) {
+			if (newDigitArr[i] != null) {
+				digitArr[i] = newDigitArr[i];
+			} else {
+				digitArr[i] = 0;
+			}
 		}
 	}
 
@@ -36,11 +46,37 @@
 	<div>
 		<div class="mode-icon">{mode}</div>
 	</div>
-	<div class="sevenSEG flex flex-row">
-		<div id="rdigit-0" class="rdigit">{digitArr[0]}</div>
-		<div id="rdigit-1" class="rdigit">{digitArr[1]}</div>
-		<div id="rdigit-2" class="rdigit">{digitArr[2]}</div>
-		<div id="rdigit-3" class="rdigit">{digitArr[3]}</div>
+	<div class="sevenSEG flex flex-row flex-wrap">
+		<div class="secondary-frequency flex flex-row">
+			<div id="secondary-rdigit-0" class="rdigit">{digitArr[0]}</div>
+			<div id="secondary-rdigit-1" class="rdigit">{digitArr[1]}</div>
+			<div id="secondary-rdigit-2" class="rdigit">{digitArr[2]}</div>
+			<div class="rdecimal-point">.</div>
+			<div id="secondary-rdigit-dp-0" class="rdigit">{digitArr[0]}</div>
+			<div id="secondary-rdigit-dp-1" class="rdigit">{digitArr[1]}</div>
+			<div id="secondary-rdigit-dp-2" class="rdigit">{digitArr[2]}</div>
+		</div>
+		<div>
+			<div class="divider-pipe">|</div>
+		</div>
+		<div class="primary-frequency flex flex-row">
+			<div id="primary-rdigit-0" class="rdigit">{digitArr[0]}</div>
+			<div id="primary-rdigit-1" class="rdigit">{digitArr[1]}</div>
+			<div id="primary-rdigit-2" class="rdigit">{digitArr[2]}</div>
+			<div class="rdecimal-point">.</div>
+			<div id="primary-rdigit-dp-0" class="rdigit">{digitArr[0]}</div>
+			<div id="primary-rdigit-dp-1" class="rdigit">{digitArr[1]}</div>
+			<div id="primary-rdigit-dp-2" class="rdigit">{digitArr[2]}</div>
+		</div>
+		<div class="terniary-frequency flex flex-row">
+			<div id="terniary-rdigit-0" class="rdigit">{digitArr[0]}</div>
+			<div id="terniary-rdigit-1" class="rdigit">{digitArr[1]}</div>
+			<div id="terniary-rdigit-2" class="rdigit">{digitArr[2]}</div>
+			<div class="rdecimal-point">.</div>
+			<div id="terniary-rdigit-dp-0" class="rdigit">{digitArr[0]}</div>
+			<div id="terniary-rdigit-dp-1" class="rdigit">{digitArr[1]}</div>
+			<div id="terniary-rdigit-dp-2" class="rdigit">{digitArr[2]}</div>
+		</div>
 	</div>
 </div>
 
@@ -68,13 +104,27 @@
 	}
 
 	.radio-segdisplay .sevenSEG {
-		font-size: 50px;
+		font-size: 35px;
 		opacity: 1;
 		margin-right: 40px;
+		width:70%
 	}
 
 	.radio-segdisplay .rdigit {
 		font-family: Segment7Standard;
+		text-algin: right;
+		padding: 8px 0px;
+	}
+
+	.radio-segdisplay .rdecimal-point {
+		font-family: Segment7Standard;
+		text-algin: right;
+		padding: 8px 0px;
+	}
+
+	.radio-segdisplay .divider-pipe {
+		margin-left: 30px;
+		margin-right: 30px;
 		text-algin: right;
 		padding: 8px 0px;
 	}
