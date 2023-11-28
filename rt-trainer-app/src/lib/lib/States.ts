60d3da12e9eb543618d243e0e10c9723d1315f56
Zac <zbenattar@gmail.com>
2023-11-28 11:05:53 +0000

    Created basic typescript types for states

---
+++ rt-trainer-app/src/lib/lib/States.ts
@@ -0,0 +1,34 @@
+export type Status = {
+    "Parked": {
+        "position": "A1",
+        "stage": "PreDepartInfo"
+    }
+};
+
+export type COMFrequency = {
+    frequency_type: "AFIS" | "TWR" | "GND",
+    frequency: number,
+    callsign: string
+};
+
+export type State = {
+    status: Status,
+    prefix: string,
+    callsign: string,
+    target_allocated_callsign: string,
+    squark: boolean,
+    current_target: COMFrequency,
+    current_radio_frequency: number,
+    current_transponder_frequency: number,
+    lat: number,
+    long: number,
+    emergency: "None" | "Mayday",
+    aircraft_type: string
+};
+
+export type StateMessageSeeds = {
+    state: State,
+    message: string,
+    scenario_seed: number,
+    weather_seed: number
+}
