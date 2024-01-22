edcd1b96e66c2fd049ddc40414af381c561ad923
Zac <zbenattar@gmail.com>
2024-01-22 11:21:14 +0000

    Package Removal
    Removed RandomSeed as it was causing an issue with the deployed version.

--- rt-trainer-app/src/lib/ts/Seed.ts
+++ rt-trainer-app/src/lib/ts/Seed.ts
@@ -1,3 +1,5 @@
+import { simpleHash, splitAndPadNumber } from './utils';
+
 export default class Seed {
 	seedString: string;
 	scenarioSeed: number;
@@ -10,30 +12,5 @@ export default class Seed {
 		const [tempScenarioSeed, tempWeatherSeed] = splitAndPadNumber(simpleHash(seedString));
 		this.scenarioSeed = Math.abs(tempScenarioSeed);
 		this.weatherSeed = Math.abs(tempWeatherSeed);
-
-		// Splits a number into two halves and pads them with zeros to make sure they are the same length
-		function splitAndPadNumber(input: number): [number, number] {
-			const numberString = input.toString();
-			const halfLength = Math.ceil(numberString.length / 2);
-			const firstHalf = parseInt(numberString.padEnd(halfLength, '0').slice(0, halfLength));
-			const secondHalf = parseInt(numberString.slice(halfLength).padEnd(halfLength, '0'));
-			return [firstHalf, secondHalf];
-		}
-
-		// Simple hash function: hash * 31 + char
-		function simpleHash(str: string): number {
-			let hash = 0;
-
-			if (str.length === 0) {
-				return hash;
-			}
-
-			for (let i = 0; i < str.length; i++) {
-				const char = str.charCodeAt(i);
-				hash = (hash << 5) - hash + char;
-			}
-
-			return hash;
-		}
 	}
 }
