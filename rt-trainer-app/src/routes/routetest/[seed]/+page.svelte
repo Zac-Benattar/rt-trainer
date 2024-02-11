19d38c4cc151fc3407cee1105d189bdb56bed070
Zac <zbenattar@gmail.com>
2024-02-11 16:27:22 +0000

    passed through gen params

--- rt-trainer-app/src/routes/routetest/[seed]/+page.svelte
+++ rt-trainer-app/src/routes/routetest/[seed]/+page.svelte
@@ -1,10 +1,44 @@
 <script lang="ts">
+	import { page } from "$app/stores";
 	import Map from "$lib/Components/Simulator/Map.svelte";
-	import { ClearSimulationStores, OpenAIPHealthStore, RouteStore, WaypointsStore } from "$lib/stores";
+	import { ClearSimulationStores, GenerationParametersStore, OpenAIPHealthStore, RouteStore, WaypointsStore } from "$lib/stores";
 	import { initiateRouteV2 } from "$lib/ts/Route";
-	import type { Waypoint } from "$lib/ts/RouteTypes";
+	import Seed from "$lib/ts/Seed";
+	import { generateRandomURLValidString } from "$lib/ts/utils";
 
 	ClearSimulationStores();
+
+		// Get the seed
+		let seedString = $page.params.seed;
+	if (seedString == null) {
+		seedString = generateRandomURLValidString(8);
+	}
+	let seed: Seed = new Seed(seedString);
+
+		// Check whether the number of airborne waypoints are specified
+		const airborneWaypointsString: string | null = $page.url.searchParams.get('airborneWaypoints');
+	let airborneWaypoints: number = 2;
+	if (
+		airborneWaypointsString == null ||
+		airborneWaypointsString == undefined ||
+		airborneWaypointsString == ''
+	) {
+		airborneWaypoints = 2;
+	} else {
+		airborneWaypoints = parseInt(airborneWaypointsString);
+		if (airborneWaypoints < 0 || airborneWaypoints > 5) {
+			airborneWaypoints = 2;
+		}
+	}
+
+	// Check whether emergencies are enabled
+	let hasEmergency: boolean = false;
+	const emergenciesString: string | null = $page.url.searchParams.get('emergencies');
+	if (emergenciesString != null) {
+		hasEmergency = emergenciesString === 'True';
+	}
+
+	GenerationParametersStore.set({seed, airborneWaypoints, hasEmergency});
 
 	initiateRouteV2();
 
