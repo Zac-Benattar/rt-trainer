d2f30ea776263954103b0ee4ccf9fa3f1343d1c9
Zac <zbenattar@gmail.com>
2024-02-14 15:45:08 +0000

    Create route endpoints
    - Created a (GET) get route by id endpoint and a (POST) create route endpoint under /api/route/[id] and /api/route respectively

--- rt-trainer-app/src/routes/api/route/[id]/+server.ts
+++ rt-trainer-app/src/routes/api/route/[id]/+server.ts
@@ -1,20 +1,22 @@
 import { db } from '$lib/db/db';
 import { routes, routePoints } from '$lib/db/schema';
 import { json } from '@sveltejs/kit';
-import { Column, sql } from 'drizzle-orm';
+import { eq } from 'drizzle-orm';
 
 export async function GET({ params }) {
 	const id: string = params.id;
 
-	const route = await db
-		.select({
-			id: routes.id,
-			name: routes.name,
-			created_at: routes.created_at,
-			updated_at: routes.updated_at,
-			points: routePoints.id
-		})
-		.from(routes)
-		.where({ id })
-		.leftJoin(routePoints, sql`${routes.id} = ${routePoints.route}`);
+	const idNumber: number = parseInt(id);
+	if (isNaN(idNumber)) {
+		return json({ error: 'Invalid route id' });
+	}
+
+	// Get the route with its route points, ordered by route point index
+	const routePointsRows = await db.query.routes.findFirst({
+		where: eq(routes.id, idNumber),
+		with: { routePoints: true },
+		orderBy: [routePoints.index]
+	});
+
+	return json(routePointsRows);
 }
