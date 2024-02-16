6ab81731958bba33c47242265d11b1ff1a4ab72d
Zac <zbenattar@gmail.com>
2024-02-16 11:55:21 +0000

    Fix aiport insert
    - All airports with their runways now insert correctly into the database
    - Frequencies not yet implemented
    - Schema updated to better match data stored

--- rt-trainer-app/src/lib/ts/OpenAIPHandler.ts
+++ rt-trainer-app/src/lib/ts/OpenAIPHandler.ts
@@ -1,6 +1,22 @@
 import axios from 'axios';
 import { OPENAIPKEY } from '$env/static/private';
-import type { AirportData, AirportReportingPointData, AirspaceData } from './AeronauticalClasses/OpenAIPTypes';
+import type {
+	AirportData,
+	AirportReportingPointData,
+	AirspaceData
+} from './AeronauticalClasses/OpenAIPTypes';
+import {
+	airportReportingPoints,
+	airports,
+	airspaces,
+	frequencies,
+	polygonPoints,
+	polygons,
+	runways
+} from '$lib/db/schema';
+import { db } from '$lib/db/db';
+import { fail } from '@sveltejs/kit';
+import { getPolygonCenter } from './utils';
 
 export async function checkSystemHealth(): Promise<string> {
 	try {
@@ -8,7 +24,7 @@ export async function checkSystemHealth(): Promise<string> {
 		return response.data.system;
 	} catch (error: unknown) {
 		console.error('Error: ', error);
-		return 'error'
+		return 'error';
 	}
 }
 
@@ -76,4 +92,169 @@ export async function getAllUKAirportReportingPoints(): Promise<AirportReporting
 		console.error('Error: ', error);
 	}
 	return [];
+}
+
+export async function updateDatabaseWithNewOpenAIPData(): Promise<
+	true | import('@sveltejs/kit').ActionFailure<{ message: string }>
+> {
+	console.log('Updating database with fresh data');
+
+	/**
+	 * Check OpenAIP system health
+	 */
+	const health = await checkSystemHealth();
+
+	if (health != 'OK') {
+		return fail(400, { message: 'OpenAIP system health not OK' });
+	}
+
+	/**
+	 * Clear airports table, runways table and frequency table
+	 */
+	await db.delete(airports);
+	await db.delete(runways);
+	await db.delete(frequencies);
+
+	/**
+	 * Fetch all UK airports
+	 */
+	const airportsData = await getAllUKAirports();
+
+	/**
+	 * Add airports to database
+	 */
+	for (let i = 0; i < airportsData.length; i++) {
+		const airportsTable = await db.insert(airports).values({
+			openaipId: airportsData[i]._id,
+			name: airportsData[i].name,
+			icaoCode: airportsData[i].icaoCode,
+			iataCode: airportsData[i].iataCode,
+			altIdentifier: airportsData[i].altIdentifier,
+			type: airportsData[i].type,
+			country: airportsData[i].country,
+			latitude: airportsData[i].geometry.coordinates[0],
+			longitude: airportsData[i].geometry.coordinates[1],
+			elevation: airportsData[i].elevation.value,
+			trafficType: airportsData[i].trafficType.join(', '),
+			ppr: airportsData[i].ppr,
+			private: airportsData[i].private,
+			skydiveActivity: airportsData[i].skydiveActivity,
+			winchOnly: airportsData[i].winchOnly
+		});
+
+		// Insert runways for the current airport
+		const runwaysToInsert = airportsData[i].runways?.map((runway) => ({
+			openaipId: runway._id,
+			airportId: parseInt(airportsTable.insertId),
+			designator: runway.designator,
+			trueHeading: runway.trueHeading,
+			alignedTrueNorth: runway.alignedTrueNorth,
+			operations: runway.operations,
+			mainRunway: runway.mainRunway,
+			turnDirection: runway.turnDirection,
+			landingOnly: runway.landingOnly,
+			takeOffOnly: runway.takeOffOnly,
+			lengthValue: runway.dimension.length.value,
+			lengthUnit: runway.dimension.length.unit,
+			widthValue: runway.dimension.width.value,
+			widthUnit: runway.dimension.width.unit,
+			toraValue: runway.declaredDistance.tora.value,
+			toraUnit: runway.declaredDistance.tora.unit,
+			todaValue: runway.declaredDistance.toda?.value,
+			todaUnit: runway.declaredDistance.toda?.unit,
+			asdaValue: runway.declaredDistance.asda?.value,
+			asdaUnit: runway.declaredDistance.asda?.unit,
+			ldaValue: runway.declaredDistance.lda.value,
+			ldaUnit: runway.declaredDistance.lda.unit,
+			thresholdLat: runway.thresholdLocation?.geometry.coordinates[0],
+			thresholdLong: runway.thresholdLocation?.geometry.coordinates[1],
+			thresholdElevationValue: runway.thresholdLocation?.elevation.value,
+			thresholdElevationUnit: runway.thresholdLocation?.elevation.unit,
+			exclusiveAircraftTypes: runway.exclusiveAircraftType?.join(', '),
+			pilotCtrlLighting: runway.pilotCtrlLighting,
+			lightingSystem: runway.lightingSystem?.join(', ')
+		}));
+		console.log(runwaysToInsert)
+		if (runwaysToInsert)
+		await db.insert(runways).values(runwaysToInsert);
+	}
+	console.log('Sucessfully inserted Airport data');
+
+	/**
+	 * Clear airspace table
+	 */
+	await db.delete(airspaces);
+
+	/**
+	 * Fetch all UK airspace
+	 */
+	const airspaceData = await getAllUKAirspace();
+
+	/**
+	 * Add airspace to database
+	 */
+	for (let i = 0; i < airspaceData.length; i++) {
+		const polygonCenter = getPolygonCenter(airspaceData[i].geometry.coordinates[0]);
+
+		const airspacesTable = await db.insert(airspaces).values({
+			openaipId: airspaceData[i]._id,
+			name: airspaceData[i].name,
+			type: airspaceData[i].type,
+			icaoClass: airspaceData[i].icaoClass,
+			activity: airspaceData[i].activity,
+			onDemand: airspaceData[i].onDemand,
+			onRequest: airspaceData[i].onRequest,
+			byNotam: airspaceData[i].byNotam,
+			specialAgreement: airspaceData[i].specialAgreement,
+			requestCompliance: airspaceData[i].requestCompliance,
+			country: airspaceData[i].country,
+			upperLimit: airspaceData[i].upperLimit.value,
+			lowerLimit: airspaceData[i].lowerLimit.value
+		});
+
+		const polygonsTable = await db.insert(polygons).values({
+			airspaceId: parseInt(airspacesTable.insertId),
+			centreLatitude: polygonCenter[0],
+			centreLongitude: polygonCenter[1]
+		});
+
+		await db.insert(polygonPoints).values(
+			airspaceData[i].geometry.coordinates[0].map((point) => ({
+				polygonId: parseInt(polygonsTable.insertId),
+				latitude: point[0].toString(),
+				longitude: point[1].toString()
+			}))
+		);
+	}
+	console.log('Sucessfully inserted Airspace data');
+
+	/**
+	 * Clear airport reporting point table
+	 */
+	await db.delete(airportReportingPoints);
+
+	/**
+	 * Fetch all UK airspace
+	 */
+	const airportReportingPointsData = await getAllUKAirportReportingPoints();
+
+	/**
+	 * Add airspace to database
+	 */
+	for (let i = 0; i < airportReportingPointsData.length; i++) {
+		await db.insert(airportReportingPoints).values({
+			openaipId: airportReportingPointsData[i]._id,
+			name: airportReportingPointsData[i].name,
+			compulsary: airportReportingPointsData[i].compulsory,
+			country: airportReportingPointsData[i].country,
+			latitude: airportReportingPointsData[i].geometry.coordinates[0],
+			longitude: airportReportingPointsData[i].geometry.coordinates[1],
+			elevation: airportReportingPointsData[i].elevation.value,
+			airports: airportReportingPointsData[i].airports.join(', ')
+		});
+	}
+
+	console.log('Sucessfully updated all OpenAIP data');
+
+	return true;
 }
--- rt-trainer-app/src/lib/ts/RouteGenerator.ts
+++ rt-trainer-app/src/lib/ts/RouteGenerator.ts
@@ -9,7 +9,8 @@ import {
 	checkSystemHealth,
 	getAllUKAirportReportingPoints,
 	getAllUKAirports,
-	getAllUKAirspace
+	getAllUKAirspace,
+	updateDatabaseWithNewOpenAIPData
 } from './OpenAIPHandler';
 import { fail } from '@sveltejs/kit';
 import {
@@ -47,169 +48,6 @@ export default class RouteGenerator {
 		return true;
 	}
 
-	public static async updateDatabaseWithNewOpenAIPData(): Promise<
-		true | import('@sveltejs/kit').ActionFailure<{ message: string }>
-	> {
-		console.log('Updating database with fresh data');
-
-		/**
-		 * Check OpenAIP system health
-		 */
-		const health = await checkSystemHealth();
-
-		if (health != 'OK') {
-			return fail(400, { message: 'OpenAIP system health not OK' });
-		}
-
-		/**
-		 * Clear airports table, runways table and frequency table
-		 */
-		await db.delete(airports);
-		await db.delete(runways);
-		await db.delete(frequencies);
-
-		/**
-		 * Fetch all UK airports
-		 */
-		const airportsData = await getAllUKAirports();
-
-		/**
-		 * Add airports to database
-		 */
-		for (let i = 0; i < airportsData.length; i++) {
-			const airportsTable = await db.insert(airports).values({
-				openaipId: airportsData[i]._id,
-				name: airportsData[i].name,
-				icaoCode: airportsData[i].icaoCode,
-				iataCode: airportsData[i].iataCode,
-				altIdentifier: airportsData[i].altIdentifier,
-				type: airportsData[i].type,
-				country: airportsData[i].country,
-				latitude: airportsData[i].geometry.coordinates[0],
-				longitude: airportsData[i].geometry.coordinates[1],
-				elevation: airportsData[i].elevation.value,
-				trafficType: airportsData[i].trafficType.join(', '),
-				ppr: airportsData[i].ppr,
-				private: airportsData[i].private,
-				skydiveActivity: airportsData[i].skydiveActivity,
-				winchOnly: airportsData[i].winchOnly
-			});
-
-			// Insert runways for the current airport
-			await db.insert(runways).values(
-				airportsData[i].runways.map((runway) => ({
-					openaipId: runway._id,
-					airportId: parseInt(airportsTable.insertId),
-					designator: runway.designator,
-					trueHeading: runway.trueHeading,
-					alignedTrueNorth: runway.alignedTrueNorth,
-					operations: runway.operations,
-					mainRunway: runway.mainRunway,
-					turnDirection: runway.turnDirection,
-					landingOnly: runway.landingOnly,
-					takeOffOnly: runway.takeOffOnly,
-					lengthValue: runway.dimension.length.value,
-					lengthUnit: runway.dimension.length.unit,
-					widthValue: runway.dimension.width.value,
-					widthUnit: runway.dimension.width.unit,
-					toraValue: runway.tora.value,
-					toraUnit: runway.tora.unit,
-					todaValue: runway.toda.value,
-					todaUnit: runway.toda.unit,
-					asdaValue: runway.asda.value,
-					asdaUnit: runway.asda.unit,
-					ldaValue: runway.lda.value,
-					ldaUnit: runway.lda.unit,
-					thresholdCoordinates: runway.threshold.coordinates,
-					elevationValue: runway.elevation.value,
-					elevationUnit: runway.elevation.unit,
-					exclusiveAircraftTypes: runway.exclusiveAircraftTypes.join(', '),
-					pilotCtrlLighting: runway.pilotCtrlLighting,
-					lightingSystem: runway.lightingSystem
-				}))
-			);
-		}
-		console.log('Sucessfully inserted Airport data');
-
-		/**
-		 * Clear airspace table
-		 */
-		await db.delete(airspaces);
-
-		/**
-		 * Fetch all UK airspace
-		 */
-		const airspaceData = await getAllUKAirspace();
-
-		/**
-		 * Add airspace to database
-		 */
-		for (let i = 0; i < airspaceData.length; i++) {
-			const polygonCenter = getPolygonCenter(airspaceData[i].geometry.coordinates[0]);
-
-			const airspacesTable = await db.insert(airspaces).values({
-				openaipId: airspaceData[i]._id,
-				name: airspaceData[i].name,
-				type: airspaceData[i].type,
-				icaoClass: airspaceData[i].icaoClass,
-				activity: airspaceData[i].activity,
-				onDemand: airspaceData[i].onDemand,
-				onRequest: airspaceData[i].onRequest,
-				byNotam: airspaceData[i].byNotam,
-				specialAgreement: airspaceData[i].specialAgreement,
-				requestCompliance: airspaceData[i].requestCompliance,
-				country: airspaceData[i].country,
-				upperLimit: airspaceData[i].upperLimit.value,
-				lowerLimit: airspaceData[i].lowerLimit.value
-			});
-
-			const polygonsTable = await db.insert(polygons).values({
-				airspaceId: parseInt(airspacesTable.insertId),
-				centreLatitude: polygonCenter[0],
-				centreLongitude: polygonCenter[1]
-			});
-
-			await db.insert(polygonPoints).values(
-				airspaceData[i].geometry.coordinates[0].map((point) => ({
-					polygonId: parseInt(polygonsTable.insertId),
-					latitude: point[0].toString(),
-					longitude: point[1].toString()
-				}))
-			);
-		}
-		console.log('Sucessfully inserted Airspace data');
-
-		/**
-		 * Clear airport reporting point table
-		 */
-		await db.delete(airportReportingPoints);
-
-		/**
-		 * Fetch all UK airspace
-		 */
-		const airportReportingPointsData = await getAllUKAirportReportingPoints();
-
-		/**
-		 * Add airspace to database
-		 */
-		for (let i = 0; i < airportReportingPointsData.length; i++) {
-			await db.insert(airportReportingPoints).values({
-				openaipId: airportReportingPointsData[i]._id,
-				name: airportReportingPointsData[i].name,
-				compulsary: airportReportingPointsData[i].compulsory,
-				country: airportReportingPointsData[i].country,
-				latitude: airportReportingPointsData[i].geometry.coordinates[0],
-				longitude: airportReportingPointsData[i].geometry.coordinates[1],
-				elevation: airportReportingPointsData[i].elevation.value,
-				airports: airportReportingPointsData[i].airports.join(', ')
-			});
-		}
-
-		console.log('Sucessfully updated all OpenAIP data');
-
-		return true;
-	}
-
 	public static async getRoute(seed: Seed): Promise<Route> {
 		const AIRCRAFT_AVERAGE_SPEED = 125; // knots
 		const NAUTICAL_MILE = 1852;
@@ -217,11 +55,11 @@ export default class RouteGenerator {
 
 		// Ensure database data is up to date - can be moved to somewhere it is run less for even faster responses
 		// Currently going to slow down all route gen methods a decent amount, please move
-		
+
 		const upToDate = await this.checkOpenAIPDataUpToDate();
-		if (!upToDate) {
+		if (upToDate) {
 			console.log('OpenAIP data is not up to date');
-			await this.updateDatabaseWithNewOpenAIPData();
+			await updateDatabaseWithNewOpenAIPData();
 		}
 
 		// Fetch all airports from the database
