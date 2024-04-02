474f89d167a9939550ffa32225cbd6912883f06f
Zac <zbenattar@gmail.com>
2024-04-02 16:38:03 +0100

    Another potential map fix

--- rt-trainer-app/src/lib/Components/Leaflet/Map.svelte
+++ rt-trainer-app/src/lib/Components/Leaflet/Map.svelte
@@ -516,6 +516,10 @@
 	});
 </script>
 
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+crossorigin=""/>
+
 <div class="w-full h-full" bind:this={mapElement}>
 	{#if map}
 		<slot />
